75bebc679f4d64cd498555d13001e98879d8c5d8
Paul Jones <pauljones23@gmail.com>
2012-11-07 11:25:51 +1000

    Updates for Redis 2.6.

--- attributes/default.rb
+++ attributes/default.rb
@@ -46,16 +46,10 @@ default['redis']['config']['loglevel'] = "warning"
 default['redis']['config']['pidfile'] = "/var/run/redis.pid"
 default['redis']['config']['rdbcompression'] = "yes"
 default['redis']['config']['timeout'] = "300"
-default['redis']['config']['vm']['enabled'] = "no"
-default['redis']['config']['vm']['max_memory'] = "0"
-default['redis']['config']['vm']['max_threads'] = "4"
-default['redis']['config']['vm']['page_size'] = "32"
-default['redis']['config']['vm']['pages'] = "134217728"
-default['redis']['config']['vm']['vm_swap_file'] = "/var/lib/redis/redis.swap"
 
 ###
 ## the following configuration settings may only work with a recent redis release
 ###
-default['redis']['config']['configure_slowlog'] = false
+default['redis']['config']['configure_slowlog'] = true
 default['redis']['config']['slowlog_log_slower_than'] = "10000"
-default['redis']['config']['slowlog_max_len'] = "1024"
+default['redis']['config']['slowlog_max_len'] = "128"
